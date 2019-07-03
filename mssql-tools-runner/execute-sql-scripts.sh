1b9ec1058f94b24c1a7a2f83f34489f8c2d8fff8
Kevin George <kgeorge.314@gmail.com>
2019-07-03 12:43:50 +0100

    Refactor - `execute-sql-scripts` to support user databases.
    Short Summary
    Previously, `execute-sql-scripts` would sequentially run all sql scripts were located
    in `mssql-tools-runner/`. This lead to the folder being unstructured, mixing user-database
    files with system database files.
    
    The restructure, now organises files their corresponding database based on folder name.
    
    Features added
    - A database is created automatically based on the folder name if it doesn’t exist
    - Password is now part of the compose file and no longer hard-coded to the script.
    - Added in some `printf` statements to do basic logging.
    
    Why is it necessary? (Bug fix, feature, improvements?)
    - Clean up and restructure of files and folders
    - To add new functionality

--- mssql-tools-runner/execute-sql-scripts.sh
+++ mssql-tools-runner/execute-sql-scripts.sh
@@ -1,4 +1,41 @@
 #!/bin/bash
-for filename in  /tsqlscripts/*.sql; do
-    /opt/mssql-tools/bin/sqlcmd -S db -U sa -P C0mplexPassw0rd! -i "$filename" ;
+
+basepathSqlScripts='/tsqlscripts'
+
+# Execute Master Scripts
+printf "[PROCESS | Sql Files for (Master) DataBase]\n"
+for tsqlFile in `find $basepathSqlScripts/master/*.sql`; do
+    
+    printf "\t|/opt/mssql-tools/bin/sqlcmd -S db -d master -U sa -i \"$tsqlFile\" ;\n"
+    /opt/mssql-tools/bin/sqlcmd -S db -d master -U sa -P "$SA_PASSWORD" -i "$tsqlFile";
+
+done
+
+# Execute User Database Files | 1:1 | Database:Folder
+for dbName in `find $basepathSqlScripts ! -path "$basepathSqlScripts" -type d -printf "%f\n"`; do
+
+    printf "[PROCESS | Sql Files for ($dbName) DataBase]\n"
+    
+    shopt -s nocasematch
+    if([[ $dbName != "master" ]]) then
+        
+        # Create Database IF NOT EXISTS
+        queryStatement="IF DB_ID('[$dbName]') IS NOT NULL CREATE DABASE [$dbName];\n"
+        q=$(printf "$queryStatement")
+        printf "\t| TRY CREATE DATABASE $dbName\n"
+        printf "\t\t|/opt/mssql-tools/bin/sqlcmd -S db -d master -U sa -P $SA_PASSWORD -q \"$q\" ;\n"
+        
+        /opt/mssql-tools/bin/sqlcmd -S db -d master -U sa -P "$SA_PASSWORD" -q "$q";
+
+
+        # Execute Database Specific Files
+        printf "\t| Execute SqlFile $dbName\n"
+        for tsqlFile in `find $basepathSqlScripts/$dbName/*.sql`; do
+
+            printf "\t\t|/opt/mssql-tools/bin/sqlcmd -S db -d $dbName -U sa -i \"$tsqlFile\" ;\n"
+            /opt/mssql-tools/bin/sqlcmd -S db -d $dbName -U sa -P "$SA_PASSWORD" -i "$tsqlFile";
+        
+        done
+    fi
+    shopt -u nocasematch
 done
